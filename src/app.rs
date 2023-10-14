82e44237d6bf530240a4e38825e84c486cd5e9e9
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2023-10-13 20:56:18 -0600

    chore(app): move to top of page

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,30 @@
+pub struct App {
+    pub current_screen: CurrentScreen, // the current screen the user is looking at, and will later determine what is rendered.
+    pub list_branches_modal: Option<Modal>,
+    pub branches: Option<BranchIterator>,
+    pub selected_branch: Option<Branch>,
+}
+
+impl App {
+    pub fn new() -> App {
+        App {
+            current_screen: CurrentScreen::Main,
+            list_branches_modal: None,
+            selected_branch: None,
+            branches: None,
+        }
+    }
+
+    pub fn toggle_branches_modal_open(&mut self) {
+        if let Some(is_open) = &self.list_branches_modal {
+            match is_open {
+                Modal::Open => self.list_branches_modal = Some(Modal::Closed),
+                Modal::Closed => self.list_branches_modal = Some(Modal::Open),
+            }
+        }
+    }
+}
+
 pub enum CurrentScreen {
     Main,
     ListingBranches,
@@ -104,33 +131,6 @@ impl BranchIterator {
         for b in self.values.iter_mut() {
             if name != b.name {
                 b.set_is_checked_out(false)
-            }
-        }
-    }
-}
-
-pub struct App {
-    pub current_screen: CurrentScreen, // the current screen the user is looking at, and will later determine what is rendered.
-    pub list_branches_modal: Option<Modal>,
-    pub branches: Option<BranchIterator>,
-    pub selected_branch: Option<Branch>,
-}
-
-impl App {
-    pub fn new() -> App {
-        App {
-            current_screen: CurrentScreen::Main,
-            list_branches_modal: None,
-            selected_branch: None,
-            branches: None,
-        }
-    }
-
-    pub fn toggle_branches_modal_open(&mut self) {
-        if let Some(is_open) = &self.list_branches_modal {
-            match is_open {
-                Modal::Open => self.list_branches_modal = Some(Modal::Closed),
-                Modal::Closed => self.list_branches_modal = Some(Modal::Open),
             }
         }
     }
